827bf25e4436581b86204f9f2cf8f764d272ba38
ramez <ramez.elzoz74@gmail.com>
2025-12-15 16:54:16 +0200

    NewSec: creat the footer sec without navigation

---
+++ src/components/layout/footer/LogoSec.jsx
@@ -0,0 +1,10 @@
+export default function LogoSec() {
+    return (
+        <div className="">
+            <img className="w-2/5 sm:w-1/3" src="images/no-bg-dark 1.png" alt="logo" />
+            <p className="text-gray-400 max-w-xs text-3xl pt-8 ps-2">
+                Building What’s Next
+            </p>
+        </div>
+    )
+}
