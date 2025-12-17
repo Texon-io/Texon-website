7764ecc67cf92e3fa8b26141c437a18482ddf9e9
Abdulrahman_Saad <abdulrahman.saad2303@gmail.com>
2025-12-17 15:33:45 +0200

    feat: projects sec waiting for paragraoh component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/layout/navbar/Navbar.jsx";
 import Footer from "@/components/layout/footer/Footer.jsx";
 import Hero from "@/sections/Hero/Hero.jsx";
 import Services from "@/sections/Services/Services.jsx";
+import Projects from "@/sections/projects/Projects.jsx";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Services />
+        <Projects/>
       <Footer />
     </>
   );
--- src/components/ui/DashedHeading.jsx
+++ src/components/ui/DashedHeading.jsx
@@ -5,7 +5,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-function DashedHeading({ heading, headClass = "", para, paraClass = "" , children}) {
+function DashedHeading({ heading, headClass = "", para, paraClass = "" , children, border=true}) {
   const wrapperRef = useRef(null);
 
   useGSAP(() => {
@@ -39,16 +39,16 @@ function DashedHeading({ heading, headClass = "", para, paraClass = "" , childre
   }, []);
 
   return (
-    <div ref={wrapperRef} className={`group py-5 border-0 border-x-0 lg:border-[1px] border-gray-500/50`}>
+    <div ref={wrapperRef} className={`group py-5 ${border && "border-0 border-x-0 lg:border-[1px] border-gray-500/50"}`}>
       <h4
-        className={`dashed-head relative font-orbitron capitalize font-semibold text-[40px] tracking-[2px] w-full opacity-0 translate-y-10 ${headClass}`}
+        className={`${headClass} dashed-head relative font-orbitron capitalize font-semibold text-4xl tracking-[4px] w-full opacity-0 translate-y-10 `}
       >
         {heading}
         <div className="absolute left-0 right-0 bottom-2 -z-10 w-full h-4 bg-accent-second/85 opacity-35 group-hover:h-10 transition-all duration-300"></div>
       </h4>
 
       <p
-        className={`dashed-para tracking-[3%] font-light w-full opacity-0 translate-y-10 ${paraClass}`}
+        className={`${paraClass} dashed-para tracking-[3%] font-light w-full opacity-0 translate-y-10`}
       >
         {para}
       </p>
